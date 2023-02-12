f3f73a628ba36cb0b7eacd8323c368bfbbd0aba3
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-02-12 09:45:15 -0600

    ✨ add api connection

--- src/screens/PokedexScreen.js
+++ src/screens/PokedexScreen.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView, Text } from 'react-native'
+import { getPokemonApi } from '../utils/api/pokemonApi'
 
 export default function PokedexScreen() {
+	const [pokemon, setPokemon] = useState([])
+
+	const fetchPokemon = async () => {
+		const { results } = await getPokemonApi()
+		setPokemon(results)
+	}
+
+	useEffect(() => {
+		fetchPokemon()
+	}, [])
+
 	return (
 		<SafeAreaView>
 			<Text>Pokedex</Text>
---
+++ src/utils/api/pokemonApi.js
@@ -0,0 +1,13 @@
+import { API_HOST } from '../constants'
+
+export const getPokemonApi = async () => {
+	const URL = `${API_HOST}/pokemon?limit=20&offset=0`
+
+	try {
+		const response = await fetch(URL)
+		const result = await response.json()
+		return result
+	} catch (error) {
+		throw error
+	}
+}
